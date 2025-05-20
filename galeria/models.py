d5012aec31b10f48a4c22475b619f9702544a0c8
Bruno Perez <braphaelperez98@gmail.com>
2025-05-20 16:18:10 -0300

    criando classes para usar no SQLITE

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+#Aqui esta nossas classes, para usarmos no banco de dados
+
+class Fotografia(models.Model):
+    nome = models.CharField(max_length= 100, null= False, blank= False) #atribuindo o atributo nome, sendo uma string de até 100 caracteres
+    legenda = models.CharField(max_length= 150, null= False, blank= False)
+    descricao = models.TextField(null= False, blank= False)
+    foto = models.CharField(max_length= 100, null= False, blank= False)
+
+    def __str__(self): #função afim de retornar o nome da fotografia, para saber se esta ok 
+        return f"Fotografia [nome{self.nome}]"
+    
+    #feito isso basta rodar python manage.py makemigrations para criar as tabelas 
