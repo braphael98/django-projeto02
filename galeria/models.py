c1d2ba146241aa1dd1f7afd028f1b785ac79388f
Bruno Perez <braphaelperez98@gmail.com>
2025-05-23 16:32:16 -0300

    Um novo campo adicionado na minha classe fotografia, em migrations

--- galeria/models.py
+++ galeria/models.py
@@ -3,8 +3,17 @@ from django.db import models
 #Aqui esta nossas classes, para usarmos no banco de dados
 
 class Fotografia(models.Model):
+    #lista de tupulas que define as opções para um campo de categoria.
+    OPCOES_CATEGORIA = [
+        ("NEBULOSA", "Nebulosa"),
+        ("ESTRELA", "Estrela"),
+        ("GALAXIA","Galaxia"),
+        ("PLANETA","Planeta"),
+    ]
+
     nome = models.CharField(max_length= 100, null= False, blank= False) #atribuindo o atributo nome, sendo uma string de até 100 caracteres
     legenda = models.CharField(max_length= 150, null= False, blank= False)
+    categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIA, default='')
     descricao = models.TextField(null= False, blank= False)
     foto = models.CharField(max_length= 100, null= False, blank= False)
 
