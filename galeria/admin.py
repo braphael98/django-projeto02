84d77718f61d0135bc8666b7d592d7d431aee2f0
Bruno Perez <braphaelperez98@gmail.com>
2025-05-23 15:55:12 -0300

    CRUD automatizado pela aba de administrador

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from galeria.models import Fotografia #todo objeto que queremos acessar pela aba de admin tem que ser importada para ca
 
-# Register your models here.
+
+class ListandoFotos(admin.ModelAdmin):
+    list_display = ("id", "nome", "legenda")#lista os itens do banco de dados
+    list_display_links = ("id", "nome") #podemos acessar os dados do banco com clicks intuivos
+    search_fields = ("nome",) #aqui é para buscar os itens com o nome
+
+admin.site.register(Fotografia, ListandoFotos) #registar a classe no django admin
