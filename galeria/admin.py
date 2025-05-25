5e747c860d3d262a811276b4fa156aad42e76623
Bruno Perez <braphaelperez98@gmail.com>
2025-05-25 17:08:29 -0300

    Novo atributo em models

--- galeria/admin.py
+++ galeria/admin.py
@@ -6,5 +6,6 @@ class ListandoFotos(admin.ModelAdmin):
     list_display = ("id", "nome", "legenda")#lista os itens do banco de dados
     list_display_links = ("id", "nome") #podemos acessar os dados do banco com clicks intuivos
     search_fields = ("nome",) #aqui é para buscar os itens com o nome
-    list_filter = ("categoria",)
+    list_filter = ("categoria",) #faz um filtro de listas em admin
+    list_per_page = 10; #numero de itens por pagina, no caso 10 
 admin.site.register(Fotografia, ListandoFotos) #registar a classe no django admin
