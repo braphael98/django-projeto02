e237e8891d7e8d0d62c2e6166d187145cf8d804a
Bruno Perez <braphaelperez98@gmail.com>
2025-05-25 17:00:36 -0300

    list filter para o admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -6,5 +6,5 @@ class ListandoFotos(admin.ModelAdmin):
     list_display = ("id", "nome", "legenda")#lista os itens do banco de dados
     list_display_links = ("id", "nome") #podemos acessar os dados do banco com clicks intuivos
     search_fields = ("nome",) #aqui é para buscar os itens com o nome
-
+    list_filter = ("categoria",)
 admin.site.register(Fotografia, ListandoFotos) #registar a classe no django admin
