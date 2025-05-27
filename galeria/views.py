35f1fe3965ccd131d6bf971fe8f8a9e2afb496ec
Bruno Perez <braphaelperez98@gmail.com>
2025-05-27 16:55:10 -0300

    orderby data_fotografia no index, funcionou

--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from galeria.models import Fotografia #Aqui importa as classes !
 
+
 def index(request):
-    fotografias = Fotografia.objects.filter(publicada = True) #agora ele mostra apenas as fotos que estao marcadas como publicadas no admin do banco de dados
+    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada = True) #ordenado pela data da fotogafia.
     return render(request, 'galeria/index.html',{"cards": fotografias})
 
 def imagem(request, foto_id):
